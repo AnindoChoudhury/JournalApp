78a007c01c37d576480403584b46537577e6b7ff
AnindoChoudhury <anindo.choudhury2004@gmail.com>
2026-04-26 16:42:18 +0530

    CRUD

--- src/main/java/com/anindo/journalapp/controllers/JournalEntryController.java
+++ src/main/java/com/anindo/journalapp/controllers/JournalEntryController.java
@@ -1,8 +1,46 @@
 package com.anindo.journalapp.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.anindo.journalapp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
+
+    Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean postJournalEntry(@RequestBody JournalEntry journalEntry){
+        long id = journalEntry.getId();
+        journalEntries.put(id,journalEntry);
+        return true;
+    }
+
+    @GetMapping("/id/{id}")
+    public JournalEntry getById(@PathVariable Long id){
+        return journalEntries.get(id);
+    }
+
+    @DeleteMapping("/id/{id}")
+    public void deleteById(@PathVariable Long id){
+        journalEntries.remove(id);
+    }
+
+    @PutMapping("/id/{id}")
+    public void putById(@PathVariable Long id,@RequestBody JournalEntry journalEntry){
+        journalEntries.put(id,journalEntry);
+    }
+
+
 
 }
--- src/main/java/com/anindo/journalapp/entity/JournalEntry.java
+++ src/main/java/com/anindo/journalapp/entity/JournalEntry.java
@@ -4,4 +4,27 @@ public class JournalEntry {
     private int id;
     private String title, content;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
