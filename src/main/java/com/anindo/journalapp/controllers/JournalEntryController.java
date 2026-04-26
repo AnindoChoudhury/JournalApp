6e76bdf8ffdedfaf0127e0e56c714c741025286d
AnindoChoudhury <anindo.choudhury2004@gmail.com>
2026-04-26 14:57:43 +0530

    Initial Setup

---
+++ src/main/java/com/anindo/journalapp/controllers/JournalEntryController.java
@@ -0,0 +1,8 @@
+package com.anindo.journalapp.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JournalEntryController {
+
+}
---
+++ src/main/java/com/anindo/journalapp/entity/JournalEntry.java
@@ -0,0 +1,7 @@
+package com.anindo.journalapp.entity;
+
+public class JournalEntry {
+    private int id;
+    private String title, content;
+
+}
