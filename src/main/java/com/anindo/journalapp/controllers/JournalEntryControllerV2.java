13b18d278a9baf3951e560966d7c759722c645e1
AnindoChoudhury <anindo.choudhury2004@gmail.com>
2026-05-02 00:58:19 +0530

    CRUD

--- src/main/java/com/anindo/journalapp/controllers/JournalEntryControllerV2.java
+++ src/main/java/com/anindo/journalapp/controllers/JournalEntryControllerV2.java
@@ -4,11 +4,7 @@ import com.anindo.journalapp.entity.JournalEntry;
 import com.anindo.journalapp.service.JournalEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/journal")
