6f884b39dbb4cb5700aee7283af92a9e805a6796
AnindoChoudhury <anindo.choudhury2004@gmail.com>
2026-06-06 16:40:19 +0530

    Updated spring security config

--- src/main/java/com/anindo/journalapp/config/SpringSecurity.java
+++ src/main/java/com/anindo/journalapp/config/SpringSecurity.java
@@ -16,14 +16,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.swing.*;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity{
 
-    @Autowired
-    private UserDetailsImp userDetailsServiceImp;
+    private final UserDetailsService userDetailsService;
+
+    public SpringSecurity(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -40,17 +45,12 @@ public class SpringSecurity{
     }
 
     @Bean
-    public DaoAuthenticationProvider authenticationProvider(UserDetailsImp userDetailsServiceImp,
-                                                            PasswordEncoder passwordEncoder){
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDeta(userDetailsServiceImp);
-        authProvider.setPasswordEncoder(passwordEncoder);
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
-    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
--- src/main/java/com/anindo/journalapp/service/UserDetailsImp.java
+++ src/main/java/com/anindo/journalapp/service/UserDetailsImp.java
@@ -2,17 +2,14 @@ package com.anindo.journalapp.service;
 
 import com.anindo.journalapp.entity.User;
 import com.anindo.journalapp.repository.UserRepository;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Setter
-@Getter
-@Component
+
+@Service
 public class UserDetailsImp implements UserDetailsService {
 
     @Autowired
@@ -22,12 +19,11 @@ public class UserDetailsImp implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if(user != null){
-            UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
+            return org.springframework.security.core.userdetails.User.builder()
                     .username(user.getUsername())
                     .password(user.getPassword())
                     .roles(user.getRoles().toArray(new String[0]))
                     .build();
-            return userDetails;
         }
 
         throw new UsernameNotFoundException("Username not found with username " + username);
